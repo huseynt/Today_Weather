a900c7ae70fd8d6d89b90276ba7e577c9010c8ac
huseynt <huseyn.tapdiqli.i@gmail.com>
2024-05-28 23:56:56 +0400

    add_info

--- JS/main.js
+++ JS/main.js
@@ -3,14 +3,22 @@ const searchWeather = document.querySelector("#searchWeather")
 var city = "baku"
 
 async function fetchWeather (city) {
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric&lang=en`)
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric&lang=az`)
     const weatherData = await data.json()
+    console.log(weatherData)
 
+    const handleDesc = ()=> {
+        return weatherData.weather[0].description.charAt(0).toUpperCase() + weatherData.weather[0].description.slice(1)
+    }
     document.querySelector("#city").innerText=weatherData.name
     const mainTemp = document.querySelector("#mainTemp").innerText=`${weatherData.main.temp}℃`
-    const info = document.querySelector("#info").innerText=weatherData.weather[0].description
-    const minMax = document.querySelector("#minMax").innerText=`min: ${weatherData.main.temp_min}℃ / max: ${weatherData.main.temp_max}℃`
-    const humidity = document.querySelector("#humidity").innerText=`humidity: ${weatherData.main.humidity}%`
+    const info = document.querySelector("#info").innerText=handleDesc()
+    const minMax = document.querySelector("#minMax").innerText=`Min: ${weatherData.main.temp_min}℃ / Max: ${weatherData.main.temp_max}℃`
+    const humidity = document.querySelector("#humidity").innerText=`Nisbi rütubət: ${weatherData.main.humidity}%`
+    const speed = document.querySelector("#speed").innerText=`
+    Külək sürəti: ${weatherData.wind.speed ? weatherData.wind.speed : "-- "}m/s
+    Külək istiqaməti: ${weatherData.wind.deg ? weatherData.wind.deg : "-- "}°
+    Atmosfer təzyiqi: ${weatherData.main.pressure ? weatherData.main.pressure : "-- "}hPa`
 }
 
 searchWeather.addEventListener("keyup", function (e){
